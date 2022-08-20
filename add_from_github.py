aab714e647d7199e8c7999e0c3ff3f2c7f69ae10
Yue-Yan <74250854+Yue-Yan@users.noreply.github.com>
2022-08-21 00:06:06 +0400

    Update add_from_github.py

--- add_from_github.py
+++ add_from_github.py
@@ -3,3 +3,16 @@
 
 def add(x, y):
   return x + y
+
+def sub(x, y):
+  return x - y
+
+def mult(x, y):
+  return x * y
+
+def dev(x, y):
+  try:
+    return x / y
+  except Exception as e:
+    print(e)
+    return None
